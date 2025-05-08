88028fd8470522d0fbb37de26bf4df7b07a00075
kayke <skayke694@gmail.com>
2025-05-08 14:35:30 +0100

    Mudei os templates coloquei o cadastrodentro das views.py

--- bolo_mania/views.py
+++ bolo_mania/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 def home_page(request):
     context = {
         "title": "Página principal",
@@ -18,5 +21,20 @@ def contact_page(request):
     context = {
         "title": "Página de contato",
         "content": "Bem-vindo a página de contato"
+    }
+    return render(request, "contact/login.html", context) 
+
+def cadastro_page(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Cadastro realizado com sucesso!")
+            return redirect("login")  # ou para onde quiser redirecionar
+    else:
+        form = UserCreationForm()
+    context = {
+        "title": "Cadastro",
+        "form": form
     }
     return render(request, "contact/view.html", context)
