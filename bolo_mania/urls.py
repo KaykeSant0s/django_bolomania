a0cce1d514cd7797b212be42d9ee07bcd23d6919
kayke <skayke694@gmail.com>
2025-05-13 10:52:18 +0100

    coloquei alguns imports no urls.py e um if para o depug

--- bolo_mania/urls.py
+++ bolo_mania/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_page, about_page,contact_page,login_page,register_page
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home_page),
@@ -28,5 +29,8 @@ urlpatterns = [
     path('login/', login_page),
     path('register/', register_page),
 
-
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
